11931fa3f385a8978d2c8ced9bf019f74287df94
crevulus <crevulus@gmail.com>
2023-02-18 16:05:17 +0000

    Firebase setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,22 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+import { firebaseApp } from "../firebase";
+import { useEffect } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+
+const auth = getAuth(firebaseApp);
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [user, loading, error] = useAuthState(auth);
+
+  // Listen to the Firebase Auth state and set the local state.
+  useEffect(() => {
+    const unregisterAuthObserver = onAuthStateChanged(auth, (userData) => {});
+    return () => unregisterAuthObserver(); // Make sure we un-register Firebase observers when the component unmounts.
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return <Component {...pageProps} />;
 }
