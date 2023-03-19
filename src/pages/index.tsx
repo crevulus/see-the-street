9330e2d3b80773e1a4690041e21d14d56cd513a3
Christopher Evans <crevulus@gmail.com>
2023-03-19 13:37:28 +0000

    test git credentials setup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <button className="btn" onClick={() => signInWithGoogle()}>Sign in</button>
+        <button className="btn" onClick={() => signInWithGoogle()}>Sign in with Google</button>
       </main>
     </>
   );
